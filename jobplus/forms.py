d563303a33ed213349fee232439c4b964e727d1c
kinglion <1609019405@qq.com>
2019-01-18 17:24:23 +0800

    注册用户

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField, IntegerField
 from wtforms.validators import DataRequired, Email, Length
+from jobplus.models import db, User
 
 
 class LoginForm(FlaskForm):
@@ -11,10 +12,22 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('用户名', validators=DataRequired())
+    username = StringField('用户名', validators=[DataRequired()])
     email = StringField('邮箱', validators=[DataRequired(), Email()])
     password = PasswordField('密码', validators=[DataRequired(), Length(6, 24)])
     repeat_password = PasswordField('重复密码', validators=[DataRequired(), Length(6, 24)])
     submit = SubmitField('提交')
 
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在')
 
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('该邮箱已被注册')
+
+    def create_user(self):
+        user = User(username=self.username.data, email=self.email.data, password=self.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return user
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from jobplus.forms import RegisterForm
 
 front = Blueprint('front', __name__)
 
@@ -6,3 +7,12 @@ front = Blueprint('front', __name__)
 @front.route('/')
 def index():
     return render_template('index.html')
+
+
+@front.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+    return render_template('register.html', form=form)
+
